43976a209ea63e3e509c2a89f2207f247f460407
zelalemmoges <157127965+zelalemmoges@users.noreply.github.com>
2025-05-22 20:20:22 +0300

    Update article.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,16 +1,13 @@
 class Article < ApplicationRecord
-#   class Article < ApplicationRecord
-#   validates :title, presence: true, uniqueness: true
-#   validates :description, presence: true
-#   validate :title_cannot_be_too_generic
+  validates :title, presence: true, uniqueness: true
+  validates :description, presence: true
+  validate :title_cannot_be_too_generic
 
-#   def title_cannot_be_too_generic
-#     if title.present? && title.downcase.strip.in?(["title", "untitled", "article"])
-#       errors.add(:title, "is too generic")
-#     end
-#   end
-# end
+  def title_cannot_be_too_generic
+    if title.present? && title.downcase.strip.in?(["title", "untitled", "article"])
+      errors.add(:title, "is too generic")
+    end
+  end
 
-validates :title, :description, presence:true
 
 end
